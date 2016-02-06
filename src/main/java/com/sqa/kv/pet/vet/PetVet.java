f366a78b140cd2308646e8944d3f0c36bc928842
Kirill <kivo35@hotmail.com>
2016-02-06 15:50:21 -0800

    Add PetVet class

---
+++ src/main/java/com/sqa/kv/pet/vet/PetVet.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.sqa.kv.pet.vet;
+
+/**
+ * @author Kirill
+ * 
+ */
+public class PetVet
+{
+	private boolean alive;
+	private String name;
+	private boolean injured;
+	private boolean male;
+	private String breed;
+}
