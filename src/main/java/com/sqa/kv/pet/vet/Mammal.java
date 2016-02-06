c987d476b1b4fa2403caa1f8b3cf2ab97ec7c544
Kirill <kivo35@hotmail.com>
2016-02-06 15:50:04 -0800

    Add Mammal subclass of PetVet class

---
+++ src/main/java/com/sqa/kv/pet/vet/Mammal.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.sqa.kv.pet.vet;
+
+/**
+ * @author Kirill
+ * 
+ */
+public class Mammal extends PetVet
+{
+	private boolean rabbiesShot;
+	private boolean houseBroken;
+}
