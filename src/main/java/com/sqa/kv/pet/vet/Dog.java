725dfb0d125d3b1dbf4cafc4db88a6546c5c379b
Kirill <kivo35@hotmail.com>
2016-02-06 15:48:56 -0800

    Add subclass Dog of class Mammal

---
+++ src/main/java/com/sqa/kv/pet/vet/Dog.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.sqa.kv.pet.vet;
+
+/**
+ * @author Kirill
+ * 
+ */
+public class Dog extends Mammal
+{
+	private boolean performsAtShows;
+	private boolean wolfBreed;
+	private String serviceType;
+}
