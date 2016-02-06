fb889d0d85dda9dd47a5567689ad8182c6c3f301
Kirill <kivo35@hotmail.com>
2016-02-06 15:50:49 -0800

    Add Reptile subclass of PetVet class

---
+++ src/main/java/com/sqa/kv/pet/vet/Reptile.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.sqa.kv.pet.vet;
+
+/**
+ * @author Kirill
+ * 
+ */
+public class Reptile extends PetVet
+{
+	private boolean scales;
+	private int heatLevel;
+	private boolean poisonous;
+}
